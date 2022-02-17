7d533f891000b5013baf9caebf7e45853314d2b0
KerboNerd <49799111+KerboNerd@users.noreply.github.com>
2022-02-17 14:16:19 +0200

    Create firstCode.py

---
+++ Programs/firstCode.py
@@ -0,0 +1,7 @@
+print("Welcome to the band name generator")
+city =  input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be " + city + " " + pet)
+print("\n")
+print("Your city choice was > " +city)
+print("Your pet choice was > " +pet)
